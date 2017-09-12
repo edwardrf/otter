51404437e5661677318b9aad42d78260291b82a5
Edward Jiang <edwardrf@gmail.com>
2017-09-12 14:48:20 +0800

    Temporary fix: onsite deployment using an older version.

--- exec/bmpcc/main.cpp
+++ exec/bmpcc/main.cpp
@@ -24,17 +24,23 @@ volatile bool is_recording = false;
 volatile int focus_pwm = 1500;
 volatile int iris_pwm = 1500;
 
+volatile int rec_action = 0;
+volatile int iris_action = 0;
+volatile int focus_action = 0;
+
 
 void handle_rec_change(volatile int last_pwm, volatile int current_pwm) {
 
   rec_pwm = (9*rec_pwm + 1*current_pwm) / 10;
 
   if (rec_pwm > 1700 && !is_recording) {
-    lanc_rec();
+    //lanc_rec();
     is_recording = true;
+    rec_action = 1;
   } else if (rec_pwm < 1200 && is_recording) {
-    lanc_rec();
+    //lanc_rec();
     is_recording = false;
+    rec_action = 1;
   }
 }
 
@@ -46,17 +52,19 @@ void handle_focus_change(volatile int last_pwm, volatile int current_pwm) {
   focus_pwm = (9*focus_pwm + 1*current_pwm) / 10;
 
   if (focus_pwm < 1200) {
-    lanc_focus_near();
+    //lanc_focus_near();
 
     if (last_focus_change == 0) {
       last_focus_change = millis();
     }
+    focus_action = 1;
   } else if (focus_pwm > 1700) {
-    lanc_focus_far();
+    //lanc_focus_far();
 
     if (last_focus_change == 0) {
       last_focus_change = millis();
     }
+    focus_action = -1;
   } else {
     last_focus_change = 0;
   }
@@ -70,19 +78,21 @@ void handle_iris_change(volatile int last_pwm, volatile int current_pwm) {
   iris_pwm = (9*iris_pwm + 1*current_pwm) / 10;
 
   if (iris_pwm < 1200) {
-    lanc_iris_dec();
+    //lanc_iris_dec();
 
     if (last_iris_change == 0) {
       // Serial.println(millis());
       last_iris_change = millis();
     }
+    iris_action = 1;
   } else if (iris_pwm > 1700) {
-    lanc_iris_inc();
+    //lanc_iris_inc();
 
     if (last_iris_change == 0) {
       // Serial.println(millis());
       last_iris_change = millis();
     }
+    iris_action = -1;
   } else {
     last_iris_change = 0;
   }
@@ -98,4 +108,27 @@ void setup() {
 }
 
 void loop() {
+  if(rec_action == 1) {
+    rec_action = 0;
+    lanc_rec();
+  }
+  delay(10);
+
+  if(iris_action == 1) {
+    iris_action = 0;
+    lanc_iris_dec();
+  }else if (iris_action == -1) {
+    iris_action = 0;
+    lanc_iris_inc();
+  }
+  delay(20);
+
+  if(focus_action == 1) {
+    focus_action = 0;
+    lanc_focus_near();
+  }else if (focus_action == -1) {
+    focus_action = 0;
+    lanc_focus_far();
+  }
+  delay(20);
 }
